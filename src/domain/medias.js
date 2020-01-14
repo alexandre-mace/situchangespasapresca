cee1f004898c1987b11f3ff1396762a721f643f3
alexandre-mace <contact.alexandremace@gmail.com>
2020-01-14 01:07:23 +0100

    Update medias

--- src/domain/medias.js
+++ src/domain/medias.js
@@ -1,43 +1,43 @@
 const medias = [
     {
-        subtitle: "Introduction",
-        source: "<iframe height=\"315\" src=\"https://www.youtube.com/embed/S1zFnV2pFPU\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
+        subtitle: "Ami des lobbies - 01/2019",
+        source: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/OIdRq5jLiIE\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
     },
     {
-        subtitle: "Introduction",
-        source: "<iframe height=\"315\" src=\"https://www.youtube.com/embed/S1zFnV2pFPU\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
+        subtitle: "Jean Jouzel, François-Marie Bréon et Jean-Baptiste Fressoz - 10/2019 - Viens voir les docteurs",
+        source: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/Rs0WvYNfREw\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
     },
     {
-        subtitle: "Introduction",
-        source: "<iframe height=\"315\" src=\"https://www.youtube.com/embed/S1zFnV2pFPU\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
+        subtitle: "Laurent Castaignède - 07/2019 - Thinkerview",
+        source: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/5gOsjIxR7T0\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
     },
     {
-        subtitle: "Introduction",
-        source: "<iframe height=\"315\" src=\"https://www.youtube.com/embed/S1zFnV2pFPU\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
+        subtitle: "Le Réveilleur - 05/2019",
+        source: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/JiZPG8tMNO0\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
     },
     {
-        subtitle: "Introduction",
-        source: "<iframe height=\"315\" src=\"https://www.youtube.com/embed/S1zFnV2pFPU\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
+        subtitle: "Aurélien Barrau - 09/2018 - Thinkerview",
+        source: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/XO4q9oVrWWw\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
     },
     {
-        subtitle: "Introduction",
-        source: "<iframe height=\"315\" src=\"https://www.youtube.com/embed/S1zFnV2pFPU\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
+        subtitle: "Pablo Servigne - 02/2018 - Thinkerview",
+        source: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/5xziAeW7l6w\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
     },
     {
-        subtitle: "Introduction",
-        source: "<iframe height=\"315\" src=\"https://www.youtube.com/embed/S1zFnV2pFPU\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
+        subtitle: "Absol Vidéos",
+        source: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/K01MnnOV-u4\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
     },
     {
-        subtitle: "Introduction",
-        source: "<iframe height=\"315\" src=\"https://www.youtube.com/embed/S1zFnV2pFPU\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
+        subtitle: "Jean-Marc Jancovici - 12/2017 - Thinkerview",
+        source: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/Fp6aJZQldFs\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
     },
     {
-        subtitle: "Introduction",
-        source: "<iframe height=\"315\" src=\"https://www.youtube.com/embed/S1zFnV2pFPU\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
+        subtitle: "Jean Ziegler - 06/2019 - Thinkerview",
+        source: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/sIJd1kuR2Ps\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
     },
     {
-        subtitle: "Introduction",
-        source: "<iframe height=\"315\" src=\"https://www.youtube.com/embed/S1zFnV2pFPU\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
+        subtitle: "Philippe Bihouix - 01/2018 - Thinkerview",
+        source: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/Bx9S8gvNKkA\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
     }
 ];
 export default medias;
